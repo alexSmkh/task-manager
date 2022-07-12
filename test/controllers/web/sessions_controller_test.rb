8bb38e38bf04ef560ae40082ed13d04e882309ea
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-07-12 17:57:38 +0300

    Fixed test inheritance and test name

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class Web::SessionControllerTest < ActionDispatch::IntegrationTest
+class Web::SessionsControllerTest < ActionController::TestCase
   test 'should get new' do
-    get new_session_path
+    get :new
     assert_response :success
   end
 end
