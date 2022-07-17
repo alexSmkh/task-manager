0255e54a545c36b45880286965aed55fce11258e
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-07-17 20:14:45 +0300

    Added index/create/update/destory/show actions for api tasks controller

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,11 +1,48 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
-  def index; end
+  def index
+    tasks = Task.all.
+      ransack(ransack_params).
+      result.
+      page(page).
+      per(per_page)
 
-  def show; end
+    respond_with(
+      tasks,
+      each_serializer: TaskSerializer,
+      root: 'items',
+      meta: build_meta(tasks),
+    )
+  end
 
-  def create; end
+  def show
+    task = Task.find(params[:id])
+    respond_with(task, serializer: TaskSerializer)
+  end
 
-  def update; end
+  def create
+    task = current_user.my_tasks.new(task_params)
+    task.save
 
-  def destroy; end
+    respond_with(task, serializer: TaskSerializer, location: nil)
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    respond_with(task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event)
+  end
 end
