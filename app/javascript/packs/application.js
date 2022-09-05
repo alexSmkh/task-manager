d057a3fa4cf5b9a0bf563206d49234299a8feaf5
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-09-05 11:32:53 +0300

    Added webpacker-react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import 'material-design-lite/material.js';
+import WebpackerReact from 'webpacker-react';
 
 Rails.start();
 ActiveStorage.start();
