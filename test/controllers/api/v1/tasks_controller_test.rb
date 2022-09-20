ddfa39ddb7d91c073f5df468d4510077597d0b6d
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-09-20 18:40:08 +0300

    Added email notification of successful task creation

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class Api::V1::TasksControllerTest < ActionController::TestCase
   test 'should get show' do
     author = create(:user)
-    task = create(task, author: author)
+    task = create(:task, author: author)
     get :show, params: { id: task.id, format: :json }
     assert_response :success
   end
@@ -20,14 +20,18 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
     assignee = create(:user)
     task_attributes = attributes_for(:task).merge({ assignee_id: assignee.id })
 
-    post :create, params: { task: task_attributes, format: :json }
+    assert_emails 1 do
+      post :create, params: { task: task_attributes, format: :json }
+    end
+
     assert_response :created
 
     data = JSON.parse(response.body)
     created_task = Task.find(data['task']['id'])
 
     assert created_task.present?
-    assert_equal task_attributes.stringify_keys, created_task.slice(*task_attributes.keys)
+    assert_equal created_task.assignee, assignee
+    assert_equal created_task.author, author
   end
 
   test 'should put update' do
