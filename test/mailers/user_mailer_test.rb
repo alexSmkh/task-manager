647b9f6663297fffd77b35abd9aaeebb6734dd68
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-09-20 19:57:30 +0300

    Added email notification of successful task update

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -7,13 +7,23 @@ class UserMailerTest < ActionMailer::TestCase
     params = { user: user, task: task }
     email = UserMailer.with(params).task_created
 
-    assert_emails 1 do
-      email.deliver_now
-    end
-
+    assert_emails(1) { email.deliver_now }
     assert_equal ['noreply@taskmanager.com'], email.from
     assert_equal [user.email], email.to
     assert_equal 'New Task Created', email.subject
     assert email.body.to_s.include?("Task #{task.id} was created")
+  end
+
+  test 'task updated' do
+    user = create(:user)
+    task = create(:task, author: user)
+    params = { user: user, task: task }
+    email = UserMailer.with(params).task_updated
+
+    assert_emails(1) { email.deliver_now }
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal [user.email], email.to
+    assert_equal 'Task Was Updated', email.subject
+    assert email.body.to_s.include?("Task #{task.id} was updated")
   end
 end
